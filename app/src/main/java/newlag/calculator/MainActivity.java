becf9cde1b1e555712791d221480666bea5f9ed2
newlag <encoderlinus@gmail.com>
2020-12-22 19:01:39 +0300

    Сверстал UI

--- app/src/main/java/newlag/calculator/MainActivity.java
+++ app/src/main/java/newlag/calculator/MainActivity.java
@@ -1,14 +1,36 @@
 package newlag.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.add_button: // Прибавить
+
+            break;
+            case R.id.sub_button: // Вычесть
+
+            break;
+            case R.id.multi_button: // Умножить
+
+            break;
+            case R.id.div_button: // Поделить
+            break;
+            case R.id.equal_button: // Результат
+            break;
+            case R.id.clear_button: // Очистить
+
+            break;
+        }
     }
 }
